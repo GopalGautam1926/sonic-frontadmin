4436181031172f3d76d240e1bc220db1f770df75
JuliusArBa <julius@arbadev.com>
2022-06-15 12:38:30 +0530

    Base URL update

--- src/services/https/httpUrl.js
+++ src/services/https/httpUrl.js
@@ -3,14 +3,14 @@ const baseUrls = {
     BASE_URL: "http://localhost:8000",
     API_URL: "http://localhost:8000",
   },
-  arba: {
-    BASE_URL: "https://sonictempserver.arba-dev.uk",
-    API_URL: "https://sonictempserver.arba-dev.uk",
-  },
   // arba: {
-  //   BASE_URL: "https://sonicserver.arba-dev.uk",
-  //   API_URL: "https://sonicserver.arba-dev.uk",
+  //   BASE_URL: "https://sonictempserver.arba-dev.uk",
+  //   API_URL: "https://sonictempserver.arba-dev.uk",
   // },
+  arba: {
+    BASE_URL: "https://sonicserver.arba-dev.uk",
+    API_URL: "https://sonicserver.arba-dev.uk",
+  },
   sonic: {
     BASE_URL: "https://apiserver.sonicdata.com",
     API_URL: "https://apiserver.sonicdata.com",
