e790056f1b9419722986ce3fc726da1fc654b261
loharsourabh <lohar1024s@gmail.com>
2022-11-21 16:38:10 +0530

    loading screen theme change

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -2,6 +2,7 @@ import { CardContent, CircularProgress } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { Card } from "@material-ui/core";
 import React from "react";
+import { useTheme } from "@material-ui/core";
 
 export default function Loading({
   contectContainerStyle,
@@ -9,23 +10,36 @@ export default function Loading({
   message,
   typographyProps,
   progressbarProps,
+  style,
   ...props
 }) {
-    return (
-      <Card elevation={elevation} {...props}>
-        <CardContent
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            minHeight:200,
-            ...contectContainerStyle
-          }}
-        >
-          <CircularProgress color="secondary" {...progressbarProps} />
-          <Typography {...typographyProps}>{message||"Please wait..."}</Typography>
-        </CardContent>
-      </Card>
-    );
+  const theme = useTheme();
+
+  return (
+    <Card
+      elevation={elevation}
+      style={{
+        ...style,
+        color: theme.palette.secondary.main,
+        background: theme.palette.background.dark3,
+      }}
+      {...props}
+    >
+      <CardContent
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: 200,
+          ...contectContainerStyle,
+        }}
+      >
+        <CircularProgress color="secondary" {...progressbarProps} />
+        <Typography {...typographyProps}>
+          {message || "Please wait..."}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
